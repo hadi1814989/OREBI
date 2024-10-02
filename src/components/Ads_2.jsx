b0f5e00cf26c685ed9dd845c63b550435be9c329
hadi1814989 <freelancer1814989@gmail.comgit config --global user.email freelancer1814989@gmail.com>
2024-10-03 00:41:10 +0600

    add

--- src/components/Ads_2.jsx
+++ src/components/Ads_2.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import ads_2 from '../assets/ads_2.png'
+import adsImage from '../assets/ads_2.png';
 
 const Ads_2 = () => {
   return (
  <section className='mb-[128px]'>
- <img className='mx-auto' src={ads_2} alt="" />
+ <img className='mx-auto' src={adsImage} alt="" />
  </section>
   )
 }
